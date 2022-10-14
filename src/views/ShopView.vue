a7d71d6752e6736c1ddb72b7c713f08cb5cb3223
daramjiyun <315ljy@gmail.com>
2022-10-14 15:21:08 +0900

    shop

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,7 +1,120 @@
 <template>
   <div>
     <NavBar></NavBar>
-    Item상점내놔
+    <div class="home">
+      <div class="container">
+        <header class="bg-dark py-5">
+          <div class="text-center text-white">
+            <h2 class="fw-bolder">Item Shop</h2>
+            <p class="lead fw-normal text-white-50 mb-0">돈 쓰세요 :)</p>
+          </div>
+        </header>
+        <section class="py-5">
+          <div class="container px-4 px-lg-5 mt-5">
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+              <div class="col mb-5">
+                <div class="card h-100">
+                  <!-- Product image-->
+                  <img class="card-img-top" src="~@/assets/item/rocket1.png" alt="..." />
+                  <!-- Product details-->
+                  <div class="card-body p-4">
+                    <div class="text-center">
+                      <!-- Product name-->
+                      <h5 class="fw-bolder">rocket1</h5>
+                      <!-- Product price-->
+                      ￦50,000
+                    </div>
+                  </div>
+                  <!-- Product actions-->
+                  <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">당장 사버렷</a></div>
+                  </div>
+                </div>
+              </div>
+              <div class="col mb-5">
+                <div class="card h-100">
+                  <!-- Sale badge-->
+                  <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
+                  <!-- Product image-->
+                  <img class="card-img-top" src="~@/assets/item/rocket2.png" alt="..." />
+                  <!-- Product details-->
+                  <div class="card-body p-4">
+                    <div class="text-center">
+                      <!-- Product name-->
+                      <h5 class="fw-bolder">rocket2</h5>
+                      <!-- Product reviews-->
+                      <div class="d-flex justify-content-center small text-warning mb-2">
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                      </div>
+                      <!-- Product price-->
+                      <span class="text-muted text-decoration-line-through">￦50,000</span>
+                      ￦45,000
+                    </div>
+                  </div>
+                  <!-- Product actions-->
+                  <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">당장 사버렷</a></div>
+                  </div>
+                </div>
+              </div>
+              <div class="col mb-5">
+                <div class="card h-100">
+                  <!-- Sale badge-->
+                  <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
+                  <!-- Product image-->
+                  <img class="card-img-top" src="~@/assets/item/rocket3.png" alt="..." />
+                  <!-- Product details-->
+                  <div class="card-body p-4">
+                    <div class="text-center">
+                      <!-- Product name-->
+                      <h5 class="fw-bolder">rocket3</h5>
+                      <!-- Product price-->
+                      <span class="text-muted text-decoration-line-through">￦50,000</span>
+                      ￦50,000
+                    </div>
+                  </div>
+                  <!-- Product actions-->
+                  <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">당장 사버렷</a></div>
+                  </div>
+                </div>
+              </div>
+              <div class="col mb-5">
+                <div class="card h-100">
+                  <!-- Product image-->
+                  <img class="card-img-top" src="~@/assets/item/rocket4.png" alt="..." />
+                  <!-- Product details-->
+                  <div class="card-body p-4">
+                    <div class="text-center">
+                      <!-- Product name-->
+                      <h5 class="fw-bolder">rocket4</h5>
+                      <!-- Product reviews-->
+                      <div class="d-flex justify-content-center small text-warning mb-2">
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                      </div>
+                      <!-- Product price-->
+                      ￦50,000
+                    </div>
+                  </div>
+                  <!-- Product actions-->
+                  <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">당장 사버렷</a></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -48,4 +161,9 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.container {
+  height: -10vh;
+  width: 70vw;
+}
+</style>
