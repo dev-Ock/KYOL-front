2c916e2657fbe411b0ee14f84ba8caf41404d344
daramjiyun <315ljy@gmail.com>
2022-12-23 15:23:24 +0900

    마우스 호버

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,27 +7,27 @@ module.exports = {
     // "plugin:vue/essential",
     'plugin:vue/recommended',
     'eslint:recommended',
-    'plugin:prettier/recommended'
+    // 'plugin:prettier/recommended'
   ],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        semi: false,
-        useTabs: false,
-        tabWidth: 2,
-        trailingComma: 'none',
-        printWidth: 120,
-        bracketSpacing: true,
-        arrowParens: 'avoid',
-        endOfLine: 'auto'
-      }
-    ],
-    'no-console': process.env.NODE_ENV === 'production' ? 'wAarn' : 'off',
+    // 'prettier/prettier': [
+    //   'error',
+    //   {
+    //     singleQuote: true,
+    //     semi: false,
+    //     useTabs: false,
+    //     tabWidth: 2,
+    //     trailingComma: 'none',
+    //     printWidth: 120,
+    //     bracketSpacing: true,
+    //     arrowParens: 'avoid',
+    //     endOfLine: 'auto'
+    //   }
+    // ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-unused-vars': ['warn', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
     'vue/multi-word-component-names': [
